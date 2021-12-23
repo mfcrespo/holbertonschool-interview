f116e89df32c714c3b9b984b98432f18d20d49d1
Maria Crespo <mafe_crespo@hotmail.com>
2021-12-23 12:09:38 -0500

    Write a function that finds all the possible substrings containing a list of words, within a given string

---
+++ 0x21-substring/README.md
@@ -0,0 +1,39 @@
+![](https://tekrants.files.wordpress.com/2016/06/str.jpg)
+
+# 0x21. Substring with concatenated words
+
+## Requirements
+
+### General
+* Allowed editors: vi, vim, emacs
+* All your files will be compiled on Ubuntu 14.04 LTS
+* Your programs and functions will be compiled with gcc 4.8.4 using the flags -Wall -Werror -Wextra and -pedantic
+* All your files should end with a new line
+* Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+* You are not allowed to use global variables
+* No more than 5 functions per file
+* In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
+* The prototypes of all your functions should be included in your header file called substring.h
+* Don’t forget to push your header file
+* All your header files should be include guarded
+
+
+### Tasks
+
+| **Filename** | **Description** |
+|---|---|
+| [0. Substring with concatenated words  ](substring.c) | Write a function that finds all the possible substrings containing a list of words, within a given string.  |
+
+#### Follow me 💬
+
+| Authors | GitHub | Twitter | Linkedin |
+| :---: | :---: | :---: | :---: |
+| Maria Fernanda Crespo | [mfcrespo](https://github.com/mfcrespo) | [@mafe_crespo](https://twitter.com/mafe_crespo) | [mariafernandacrespo](https://www.linkedin.com/in/mariafernandacrespo) |
+
+### License
+*`0x21. Substring with concatenated words` is open source and therefore free to download and use without permission.*
+
+<a href="url"><img src="https://www.holbertonschool.com/holberton-logo.png" align="middle" width="100" height="30"></a>
+
+##### Holberton School – Specializations - Interview Preparation ― Algorithms
+##### December, 2021. Cali, Colombia
---
+++ 0x21-substring/substring.c
@@ -0,0 +1,123 @@
+#include "substring.h"
+/**
+ * value_cmp - value comparator
+ * @a: int
+ * @b: int
+ * Return: 
+ **/
+
+int value_cmp(const void *a, const void *b)
+{
+    int value1 = *(int *)a;
+    int value2 = *(int *)b;
+
+    return (value1 - value2);
+}
+
+/**
+ * check - function check word in string
+ * @s: char
+ * @words: array of char
+ * @nb_words: int
+ * Return: int
+ **/
+
+int *check(char const *s, char const **words, int nb_words)
+{
+    int *arr, i, j = 0, len;
+    char *aux;
+
+    arr = malloc((sizeof(int) * nb_words));
+    if (!arr)
+        return (NULL);
+    for (i = 0; i < nb_words; i++)
+    {
+        aux = strstr(s, words[i]);
+        if (!aux)
+        {
+            free(arr);
+            return (NULL);
+        }
+        len = (int)strlen(aux);
+        while (j < i)
+        {
+            if (len == arr[j])
+            {
+                aux = strstr(s + (int)strlen(s) - len + 1, words[i]);
+                if (!aux)
+                {
+                    free(arr);
+                    return (NULL);
+                }
+                len = (int)strlen(aux);
+                arr[i] = len;
+            }
+            j++;
+        }
+        arr[i] = len;
+    }
+    return (arr);
+}
+
+/**
+ * find_substring - function that finds all the possible substrings
+ * containing a list of words, within a given string.
+ * @s: it's the string to scan
+ * @words: it's the array of words all substrings must be a concatenation
+ * arrangement of
+ * @nb_words: it's the number of elements in the array words
+ * @n: hold the address at which to store the number of elements in the
+ * returned array.
+ * Return:  an allocated array, storing each index in s, at which a substring
+ * was found. If no solution is found, NULL can be returned
+ */
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+    int *index, *arr, len, diff, s_len, idx, tmp;
+    int i, j;
+
+    *n = 0;
+    len = (int)strlen(words[0]);
+    s_len = (int)strlen(s);
+    for (j = 0; j < s_len; j++)
+    {
+        arr = check(s, words, nb_words);
+        if (!arr)
+            break;
+        qsort(arr, nb_words, sizeof(int), value_cmp);
+        for (i = 0; i < nb_words - 1; i++)
+        {
+            if (arr[i + 1] - arr[i] != len)
+            {
+                idx = 0;
+                break;
+            }
+            else
+                idx = 1;
+        }
+        tmp = arr[nb_words - 1];
+        free(arr);
+        if (idx == 1)
+        {
+            diff = s_len - tmp;
+            if (*n == 0 || diff != index[*n - 1])
+            {
+                if (*n == 0)
+                {
+                    index = malloc((sizeof(int)));
+                    if (!index)
+                        return (NULL);
+                }
+                else
+                    index = realloc(index, sizeof(int) * (*n + 1));
+                index[*n] = diff;
+                *n += 1;
+            }
+        }
+        s++;
+    }
+    if (*n == 0)
+        return (NULL);
+    return (index);
+}
