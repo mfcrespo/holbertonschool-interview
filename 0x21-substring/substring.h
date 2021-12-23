20bb7adfdd87d834438fbd564f3115269950f451
Maria Crespo <mafe_crespo@hotmail.com>
2021-12-23 12:54:55 -0500

    Fix betty style

--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -1,6 +1,9 @@
 #ifndef _SUBSTRING_H_
 #define _SUBSTRING_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 
